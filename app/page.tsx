47df0786d94104cfb14e5257b22183f1507a7325
Neil <neil@roosterdesign.co.uk>
2025-02-05 15:02:56 +0000

    remove unwanted code - PRE PAYALOAD

--- app/page.tsx
+++ app/page.tsx
@@ -15,44 +15,12 @@ export default function Home() {
             <CategoryGrid />
             <Testimonials />
             <Banner />
-
             <GalleryTextBlock buttonLabel="Browse Products" buttonLink="#" title="Pellentesque sagittis accumsan tellus.">
                 <>
                     <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less.</p>
                     <p>A domain name is one of the first steps to establishing your brand. Secure a consistent brand image with a domain name that matches your business.</p>
                 </>
             </GalleryTextBlock>
-
-            {/* <Container>
-
-                <br /><br /><br /><br />
-
-                <h1>This is a heading 1</h1>
-                <h2>This is a heading 2</h2>
-                <h3>This is a heading 3</h3>
-                <h4>This is a heading 4</h4>
-                <h5>This is a heading 5</h5>
-                <h6>This is a heading 6</h6>
-                <p>Lorem ipsum dolor sit amet, <a href="#">consectetur adipiscing elit</a>. Donec vitae nunc eu risus suscipit varius quis nec ipsum. Donec vitae ante id arcu fermentum auctor. Mauris fermentum purus est, nec fringilla dolor pretium sed. Sed pretium pulvinar varius. Praesent in dignissim dolor. Nulla facilisi. Maecenas lobortis sapien id enim tempor varius. Nunc at augue leo. Nam dapibus aliquet tincidunt. Cras venenatis nisi nisi, eu efficitur dui consectetur volutpat. Nunc elit metus, rhoncus vitae posuere in, lacinia id dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
-                <p>Aliquam placerat tincidunt turpis, et congue nibh tincidunt eget. Fusce scelerisque turpis id tristique molestie. Nullam elementum, lacus eget feugiat commodo, lorem massa laoreet lorem, quis imperdiet nulla nisi semper leo. Cras posuere lorem vitae massa viverra rhoncus. In quis sodales urna. Donec libero leo, interdum et interdum at, commodo nec eros. Ut molestie dictum arcu at placerat. Vivamus quis eros orci. Pellentesque ullamcorper eleifend nisl nec dapibus.</p>
-                <p>Aliquam placerat tincidunt turpis, et congue nibh tincidunt eget. Fusce scelerisque turpis id tristique molestie. Nullam elementum, lacus eget feugiat commodo, lorem massa laoreet lorem, quis imperdiet nulla nisi semper leo. Cras posuere lorem vitae massa viverra rhoncus. In quis sodales urna. Donec libero leo, interdum et interdum at, commodo nec eros. Ut molestie dictum arcu at placerat. Vivamus quis eros orci. Pellentesque ullamcorper eleifend nisl nec dapibus.</p>
-                <p>Aliquam placerat tincidunt turpis, et congue nibh tincidunt eget. Fusce scelerisque turpis id tristique molestie. Nullam elementum, lacus eget feugiat commodo, lorem massa laoreet lorem, quis imperdiet nulla nisi semper leo. Cras posuere lorem vitae massa viverra rhoncus. In quis sodales urna. Donec libero leo, interdum et interdum at, commodo nec eros. Ut molestie dictum arcu at placerat. Vivamus quis eros orci. Pellentesque ullamcorper eleifend nisl nec dapibus.</p>
-                <p>Aliquam placerat tincidunt turpis, et congue nibh tincidunt eget. Fusce scelerisque turpis id tristique molestie. Nullam elementum, lacus eget feugiat commodo, lorem massa laoreet lorem, quis imperdiet nulla nisi semper leo. Cras posuere lorem vitae massa viverra rhoncus. In quis sodales urna. Donec libero leo, interdum et interdum at, commodo nec eros. Ut molestie dictum arcu at placerat. Vivamus quis eros orci. Pellentesque ullamcorper eleifend nisl nec dapibus.</p>
-                <ul>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
-                </ul>
-                <ol>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
-                </ol>
-
-                <a href="#" className="btn">Click me</a>
-
-            </Container> */}
-
         </>
     );
 }
