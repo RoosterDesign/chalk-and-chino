26be9e104045e0c17feb904d5ade79a2b369a26e
Neil <neil@roosterdesign.co.uk>
2025-07-09 18:21:14 +0100

    gallery potential fix

--- src/lib/pages/getPageBySlug.ts
+++ src/lib/pages/getPageBySlug.ts
@@ -1,7 +1,8 @@
-import configPromise from '@/payload.config';
 import { draftMode } from 'next/headers';
 import { getPayload, type RequiredDataFromCollectionSlug } from 'payload';
 import { cache } from 'react';
+
+import configPromise from '@/payload.config';
 
 export const getPageBySlug = cache(
     async (slug: string): Promise<null | RequiredDataFromCollectionSlug<'pages'>> => {
@@ -12,6 +13,7 @@ export const getPageBySlug = cache(
             collection: 'pages',
             draft,
             limit: 1,
+            depth: 2,
             pagination: false,
             overrideAccess: draft,
             where: {
