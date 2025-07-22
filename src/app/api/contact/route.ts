fd816f2f1f88befbe7f36a8b4bf386505f9b506c
Neil <neil@roosterdesign.co.uk>
2025-07-22 08:45:08 +0100

    brevo smtp

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -61,6 +61,7 @@ export async function POST(req: NextRequest) {
             host: process.env.EMAIL_HOST,
             port: Number(process.env.EMAIL_PORT),
             secure: false,
+            requireTLS: true,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
