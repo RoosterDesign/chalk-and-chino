562d3e1e144a8e3b896034783cbd6a65f974c7c6
Neil <neil@roosterdesign.co.uk>
2025-07-23 09:08:00 +0100

    contact setup update

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -61,7 +61,6 @@ export async function POST(req: NextRequest) {
             host: process.env.EMAIL_HOST,
             port: Number(process.env.EMAIL_PORT),
             secure: false,
-            requireTLS: true,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
