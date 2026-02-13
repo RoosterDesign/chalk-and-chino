4751b7a009ac6af4a989fd608eed7f6e4ee41410
Neil <neil.dewing@happydance.love>
2026-02-13 09:22:55 +0000

    image update

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -27,60 +27,36 @@ export const Media: CollectionConfig = {
         width: 1920,
         height: 300,
         fit: 'cover',
-        formatOptions: {
-          format: 'avif',
-          options: { quality: 90 },
-        },
       },
       {
         name: 'portrait',
         width: 400,
         height: 500,
         fit: 'cover',
-        formatOptions: {
-          format: 'avif',
-          options: { quality: 90 },
-        },
       },
       {
         name: 'landscape',
         width: 1920,
         height: 900,
         fit: 'cover',
-        formatOptions: {
-          format: 'avif',
-          options: { quality: 90 },
-        },
       },
       {
         name: 'categoryBanner',
         width: 990,
         height: 325,
         fit: 'cover',
-        formatOptions: {
-          format: 'avif',
-          options: { quality: 90 },
-        },
       },
       {
         name: 'thumbnail',
         width: 820,
         height: 615,
         fit: 'cover',
-        formatOptions: {
-          format: 'avif',
-          options: { quality: 90 },
-        },
       },
       {
         name: 'modalPreview',
         width: 1440,
         height: 1100,
         fit: 'cover',
-        formatOptions: {
-          format: 'avif',
-          options: { quality: 90 },
-        },
       },
     ],
 
